24f6c6e147787b91a055b7759ec944826b6956da
denys-develop <denys.tsuprenko@yandex.ru>
2022-08-17 21:41:41 +0300

    anchor section

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ $('.slider__btn--next').click(function() {
     owl.trigger('next.owl.carousel');
 })
 
+
+
+// Anchor 
+const anchor = document.querySelectorAll(".nav__list a");
+console.log(anchor);
+
+anchor.forEach(anc => {
+    anc.addEventListener("click", function(event){
+        event.preventDefault();
+
+        const id = anc.getAttribute("href");
+        const elem = document.querySelector(id);
+
+        window.scroll({
+            top: elem.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+}); 
